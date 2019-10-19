d195f35a321b2222ba6899b7d147eeeff3259fde
kamalkraj <kamalraj97@gmail.com>
2019-10-19 13:37:45 -0700

    model parameter update and docs

--- model.py
+++ model.py
@@ -10,21 +10,35 @@ from utils import tf_utils
 
 class BertNer(tf.keras.Model):
 
-    def __init__(self, bert_path, float_type, num_labels, max_seq_length, final_layer_initializer=None):
+    def __init__(self, bert_model,float_type, num_labels, max_seq_length, final_layer_initializer=None):
+        '''
+        bert_model : string or dict
+                     string: bert pretrained model directory with bert_config.json and bert_model.ckpt
+                     dict: bert model config , pretrained weights are not restored
+        float_type : tf.float32
+        num_labels : num of tags in NER task
+        max_seq_length : max_seq_length of tokens
+        final_layer_initializer : default:  tf.keras.initializers.TruncatedNormal
+        '''
         super(BertNer, self).__init__()
         
         input_word_ids = tf.keras.layers.Input(shape=(max_seq_length,), dtype=tf.int32, name='input_word_ids')
         input_mask = tf.keras.layers.Input(shape=(max_seq_length,), dtype=tf.int32, name='input_mask')
         input_type_ids = tf.keras.layers.Input(shape=(max_seq_length,), dtype=tf.int32, name='input_type_ids')
+        
+        if type(bert_model) == str:
+            bert_config = BertConfig.from_json_file(os.path.join(bert_model,"bert_config.json"))
+        elif type(bert_model) == dict:
+            bert_config = BertConfig.from_dict(bert_model)
 
-        bert_config = BertConfig.from_json_file(os.path.join(bert_path,"bert_config.json"))
         bert_layer = BertModel(config=bert_config,float_type=float_type)
 
         _, sequence_output = bert_layer(input_word_ids, input_mask,input_type_ids)
         self.bert = tf.keras.Model(inputs=[input_word_ids, input_mask, input_type_ids],outputs=[sequence_output])
-        init_checkpoint = os.path.join(bert_path,"bert_model.ckpt")
-        checkpoint = tf.train.Checkpoint(model=self.bert)
-        checkpoint.restore(init_checkpoint).assert_existing_objects_matched()
+        if type(bert_model) == str:
+            init_checkpoint = os.path.join(bert_model,"bert_model.ckpt")
+            checkpoint = tf.train.Checkpoint(model=self.bert)
+            checkpoint.restore(init_checkpoint).assert_existing_objects_matched()
         
         if final_layer_initializer is not None:
             initializer = final_layer_initializer
